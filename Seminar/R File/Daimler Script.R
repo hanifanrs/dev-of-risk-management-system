ddb9095507c2843752f0013c4981f08e89840495
Hanifan Shiddiqi <49444745+hanifanrs@users.noreply.github.com>
2022-11-10 15:04:30 +0100

    Update Daimler Script.R

--- Seminar/R File/Daimler Script.R
+++ Seminar/R File/Daimler Script.R
@@ -17,38 +17,46 @@ daimler$Datum <- as.Date(daimler$Datum, "%Y-%m-%d")
 daimler <- daimler[-c(2:4,6:7)]
 
 # Calculating logarithmic returns
-n <- length(daimler$Schlusskurs)
-logreturn <- log(daimler$Schlusskurs[-1]/daimler$Schlusskurs[-n])
+n_mbg <- length(daimler$Schlusskurs)
+logreturn_mbg <- log(daimler$Schlusskurs[-1]/daimler$Schlusskurs[-n_mbg])
 
 
 # Convert the returns to Data Frame to better view
-logreturn.df <- data.frame(logreturn)
-sortedlog <- data.frame(logreturn.df[order(logreturn),]) %>% 
+logreturndf_mbg <- data.frame(logreturn_mbg)
+sortedlog_mbg <- data.frame(logreturndf_mbg[order(logreturn_mbg),]) %>% 
   rename(logreturns = 1)
 
 # Calculate the portfolio (assume that we have 1000 shares in one portfolio)
-lastprice <- head(daimler$Schlusskurs, n=1)
-shares <- 1000
-portfoliovalue <- lastprice*shares
-minimum <- min(sortedlog)
-maximum <- max(sortedlog)
+lastprice_mbg <- head(daimler$Schlusskurs, n=1)
+shares_mbg <- 1000
+portfoliovalue_mbg <- lastprice_mbg*shares_mbg
+minimum_mbg <- min(sortedlog_mbg)
+maximum_mbg <- max(sortedlog_mbg)
 
 # Plot the sorted return
 library(tidyverse)
-ggplot(sortedlog, aes(x = logreturns)) +
+ggplot(sortedlog_mbg, aes(x = logreturns)) +
   geom_freqpoly() +
   labs(title = "Daily price changes")
 
 # fifth percentile (Confidence Interval = 95%)
-var95 <- unname(quantile(sortedlog$logreturns, c(.05)))
-worstcase_ninetyfive <- var95*portfoliovalue
+var95_mbg <- unname(quantile(sortedlog_mbg$logreturns, c(.05)))
+worstcase_ninetyfive_mbg <- var95_mbg*portfoliovalue_mbg
 
 # first percentile (Confidence Interval = 99%)
-var99 <- unname(quantile(sortedlog$logreturns, c(.01)))
-worstcase_ninetynine <- var99*portfoliovalue
+var99_mbg <- unname(quantile(sortedlog_mbg$logreturns, c(.01)))
+worstcase_ninetynine_mbg <- var99_mbg*portfoliovalue_mbg
 
 # Compare to package in CRAN
-install.packages("PerformanceAnalytics")
+#install.packages("PerformanceAnalytics")
 library(PerformanceAnalytics)
-VaR(logreturn, p = 0.95, method = "historical")
-VaR(logreturn, p = 0.99, method = "historical")
+VaR(logreturn_mbg, p = 0.95, method = "historical")
+VaR(logreturn_mbg, p = 0.99, method = "historical")
+
+# Compare the Value at Risk
+zusammenfassung <- data.frame(
+  stock = c("Daimler", "Deutsche Bank"),
+  portfolio_value = c(portfoliovalue_mbg, portfoliovalue_dbk),
+  value_at_risk = c(var99_mbg,var99_dbk ),
+  worst_case = c(worstcase_ninetynine_mbg,worstcase_ninetynine_dbk ))
+
