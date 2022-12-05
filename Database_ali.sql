e59038416859b6c976f1b5ae440c1d8d21b67297
AliNaji00 <117733375+AliNaji00@users.noreply.github.com>
2022-12-05 13:47:52 +0100

    Add files via upload

--- Database_ali.sql
+++ Database_ali.sql
@@ -1,20 +1,50 @@
 use merc;
 
-drop table if exists fonds;
 drop table if exists portfolio;
+drop table if exists fonds;
 
+
+
+drop view if exists ausgabe;
 
 create table Fonds (
 ID  Integer  Primary Key,
-Name  Char(30)  Not null,
 ISIN  Char(30)  Not null,
-Währung  Integer  Not null
+Name  Char(30)  Not null,
+Währung  Char(30)  Not null
 ); 
 
 create table Portfolio (
-ID  Integer  Primary Key,
+ID  Integer,
 Anzahl_Aktien  Integer  Not null,
 Datum   date  not null,
-Wert_Portfolio  Integer  Not Null
+Wert_Portfolio  Integer  Not Null,
+
+Constraint FK_Portfolio_Fonds FOREIGN KEY (ID) REFERENCES Fonds(ID)		
+						ON DELETE CASCADE ON UPDATE CASCADE
 );
+
+insert into fonds
+Values (123, 'allianz', 'DE12345678910', 'EUR');
+insert into fonds
+Values (456, 'DWS', 'DE6592356017', 'EUR');
+insert into fonds
+Values (789, 'unifonds', 'DE0987654321', 'EUR');
+
+insert into portfolio
+Values (123,5,'2022-11-22',10);
+insert into portfolio 
+Values (456,7,'2022-11-23',8);
+insert into portfolio 
+Values (789,15,'2022-11-24',6);
+ 
+
+CREATE VIEW Ausgabe
+AS SELECT anzahl_aktien as 'Aktienmenge', wert_portfolio as 'Portfoliowert'  from portfolio;
+
+
+
+
+
+
  
