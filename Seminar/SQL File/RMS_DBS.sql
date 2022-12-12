abf7b2782d4fb41db3d00aff9943ad61650d0a70
Hanifan Shiddiqi <49444745+hanifanrs@users.noreply.github.com>
2022-12-12 13:27:59 +0100

    Update RMS_DBS.sql

--- Seminar/SQL File/RMS_DBS.sql
+++ Seminar/SQL File/RMS_DBS.sql
@@ -9,53 +9,58 @@ DROP TABLE IF EXISTS Tagespreis;
 DROP TABLE IF EXISTS Aktien;
 
 # Anlegen der Tabellen:
+
 CREATE TABLE Aktien (
     Aktien_id 				INT NOT NULL AUTO_INCREMENT,
-    Name 				VARCHAR(60),
-    ISIN 				VARCHAR(12) NOT NULL UNIQUE,
-    WKN					VARCHAR(6) NOT NULL UNIQUE,
-    Branche				VARCHAR(30),
-    Land				VARCHAR(60),					
+    Name 					VARCHAR(60),
+    ISIN 					VARCHAR(12) NOT NULL UNIQUE,
+    WKN						VARCHAR(6) NOT NULL UNIQUE,
+    Branche					VARCHAR(30),
+    Land					VARCHAR(60),					
     PRIMARY KEY (Aktien_id)
 );
 
 CREATE TABLE Tagespreis (
     Tagespreis_id 			INT NOT NULL AUTO_INCREMENT,
     Aktien_id 				INT NOT NULL,
-    Datum 				DATE NOT NULL,
-    Erster 				DECIMAL(10,2) NOT NULL,
-    Hoch 				DECIMAL(10,2) NOT NULL,
-    Tief 				DECIMAL(10,2) NOT NULL,
+    Datum 					DATE NOT NULL,
+    Erster 					DECIMAL(10,2) NOT NULL,
+    Hoch 					DECIMAL(10,2) NOT NULL,
+    Tief 					DECIMAL(10,2) NOT NULL,
     Schlusskurs 			DECIMAL(10,2) NOT NULL,
     Stuecke 				INT NOT NULL,
     Volumen 				INT NOT NULL,
     PRIMARY KEY (Tagespreis_id),
-	Constraint FK_Aktien_id FOREIGN KEY (Aktien_id) REFERENCES Aktien(Aktien_id) ON DELETE NO ACTION ON UPDATE NO ACTION
+	Constraint FK_Aktien_id FOREIGN KEY (Aktien_id) REFERENCES Aktien(Aktien_id)
+								ON DELETE NO ACTION ON UPDATE NO ACTION
 );
 
 CREATE TABLE Snapshot (
     Snapshot_id 			INT NOT NULL,
-    Anzahl 				INT NOT NULL,
+    Anzahl 					INT NOT NULL,
     Aktien_id 				INT NOT NULL,
     PRIMARY KEY (Snapshot_id, Aktien_id),
-	Constraint FK_Aktien_id2 FOREIGN KEY (Aktien_id) REFERENCES Aktien(Aktien_id) ON DELETE NO ACTION ON UPDATE NO ACTION
+	Constraint FK_Aktien_id2 FOREIGN KEY (Aktien_id) REFERENCES Aktien(Aktien_id)
+								ON DELETE NO ACTION ON UPDATE NO ACTION
 );
 
 CREATE TABLE Portfolio (
     Portfolio_id 			INT NOT NULL,
-    Datum 				DATE NOT NULL,
+    Datum 					DATE NOT NULL,
     Snapshot_id 			INT NOT NULL,
     Value_at_risk 			DECIMAL(10,2),
-    Wert 				DECIMAL(10,2),
+    Wert 					DECIMAL(10,2),
     PRIMARY KEY (Portfolio_id, Snapshot_id),
-	Constraint FK_Snapshot_id FOREIGN KEY (Snapshot_id) REFERENCES Snapshot(Snapshot_id) ON DELETE NO ACTION ON UPDATE NO ACTION
+	Constraint FK_Snapshot_id FOREIGN KEY (Snapshot_id) REFERENCES Snapshot(Snapshot_id)
+								ON DELETE NO ACTION ON UPDATE NO ACTION
 );
 
 CREATE TABLE Fonds (
     Fonds_id 				INT NOT NULL AUTO_INCREMENT,
-    Name 				VARCHAR(60),
-    ISIN 				VARCHAR(12) NOT NULL UNIQUE,
+    Name 					VARCHAR(60),
+    ISIN 					VARCHAR(12) NOT NULL UNIQUE,
     Portfolio_id 			INT NOT NULL,
     PRIMARY KEY (Fonds_id),
-    Constraint FK_Portfolio_id FOREIGN KEY (Portfolio_id) REFERENCES Portfolio(Portfolio_id) ON DELETE NO ACTION ON UPDATE NO ACTION
+    Constraint FK_Portfolio_id FOREIGN KEY (Portfolio_id) REFERENCES Portfolio(Portfolio_id)
+								ON DELETE NO ACTION ON UPDATE NO ACTION
 );
