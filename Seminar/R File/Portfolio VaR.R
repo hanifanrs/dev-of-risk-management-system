5bdf5a372181f7b76ebe907c80f4407e462423ec
Hanifan Shiddiqi <49444745+hanifanrs@users.noreply.github.com>
2023-01-18 12:58:42 +0100

    Update Portfolio VaR.R

--- Seminar/R File/Portfolio VaR.R
+++ Seminar/R File/Portfolio VaR.R
@@ -2,7 +2,7 @@
 #mbg = Mercedes-Benz Group AG
 library(dplyr)
 library(DBI)
-library(lubridate) 
+library(lubridate)
 
 # Check data type
 str(daimler)
@@ -20,39 +20,50 @@ deutschebank <- subset(deutschebank, year(deutschebank$Datum) == 2022)
 daimler <- daimler %>% arrange_all(desc)
 deutschebank <- deutschebank %>% arrange_all(desc)
 
-# Number of today's shares
-daimler_anzahl_heute <- dbGetQuery(rms_dbs,"SELECT s.Anzahl FROM Portfolio p
-INNER JOIN Snapshot s ON s.Snapshot_id = p.Snapshot_id
-INNER JOIN Tagespreis t ON t.Aktien_id = s.Aktien_id AND t.Datum = p.Datum
-WHERE t.Datum = '2022-12-30' AND p.Snapshot_id = 257 AND t.Aktien_id = 1;")
-daimler_anzahl_heute <- as.matrix(daimler_anzahl_heute)
+# Calculating logarithmic returns (Rendite Prozent)
+n_mbg <- length(daimler$Schlusskurs)
+daimler_rendite <- log(daimler$Schlusskurs[-1]/daimler$Schlusskurs[-n_mbg])
 
-deutschebank_anzahl_heute <- dbGetQuery(rms_dbs,"SELECT s.Anzahl FROM Portfolio p
-INNER JOIN Snapshot s ON s.Snapshot_id = p.Snapshot_id
-INNER JOIN Tagespreis t ON t.Aktien_id = s.Aktien_id AND t.Datum = p.Datum
-WHERE t.Datum = '2022-12-30' AND p.Snapshot_id = 257 AND t.Aktien_id = 2;")
-deutschebank_anzahl_heute <- as.matrix(deutschebank_anzahl_heute)
+n_dbk <- length(deutschebank$Schlusskurs)
+deutschebank_rendite <- log(deutschebank$Schlusskurs[-1]/deutschebank$Schlusskurs[-n_dbk])
 
-# Calculate the Value at Risk
-daimler_var <- t(daimler_anzahl_heute %*% daimler$Schlusskurs)
-daimler_var[1,] <- 0
-deutschebank_var <- t(deutschebank_anzahl_heute %*% deutschebank$Schlusskurs)
-deutschebank_var[1,] <- 0
-var_berechnen <- daimler_var+deutschebank_var
-var_berechnen <- sort(var_berechnen, decreasing = TRUE)
-
-# Calculate the Value of Portfolio
-wert_berechnen <- dbGetQuery(rms_dbs, 
-"SELECT SUM(s.Anzahl * t.Schlusskurs) AS Wert
+# Number of shares
+anzahl <- dbGetQuery(rms_dbs,
+"SELECT p.Datum, s.Anzahl, t.Aktien_id
 FROM Portfolio p
 INNER JOIN Snapshot s ON s.Snapshot_id = p.Snapshot_id
-INNER JOIN Tagespreis t ON t.Aktien_id = s.Aktien_id AND t.Datum = p.Datum
-GROUP BY s.Snapshot_id;")
-wert_berechnen <- as.numeric(wert_berechnen$Wert)
+INNER JOIN Tagespreis t ON t.Aktien_id = s.Aktien_id AND t.Datum = p.Datum;")
+
+daimler_anzahl <- subset(anzahl, anzahl$Aktien_id == 1)
+daimler_anzahl <- daimler_anzahl %>% arrange_all(desc)
+daimler_anzahl_heute <- daimler_anzahl[1,2]
+daimler_anzahl_heute <- as.matrix(daimler_anzahl_heute)
+
+deutschebank_anzahl <- subset(anzahl, anzahl$Aktien_id == 2)
+deutschebank_anzahl <- deutschebank_anzahl %>% arrange_all(desc)
+deutschebank_anzahl_heute <- deutschebank_anzahl[1,2]
+deutschebank_anzahl_heute <- as.matrix(deutschebank_anzahl_heute)
+
+# Wert
+daimler_wert <- daimler$Schlusskurs * daimler_anzahl$Anzahl
+deutschebank_wert <- deutschebank$Schlusskurs * deutschebank_anzahl$Anzahl
+
+# Calculate the Value of Portfolio
+portfolio_r <- daimler_wert+deutschebank_wert
+
+# Calculate the what would have been the portfolio value yesterday
+daimler_portfolio_gestern <- t(daimler_anzahl_heute %*% daimler$Schlusskurs)
+daimler_portfolio_gestern[1,] <- 0
+deutschebank_portfolio_gestern <- t(deutschebank_anzahl_heute %*% deutschebank$Schlusskurs)
+deutschebank_portfolio_gestern[1,] <- 0
+portfolio_gestern <- daimler_portfolio_gestern+deutschebank_portfolio_gestern
+
+#portfolio_gestern <- sort(portfolio_gestern, decreasing = TRUE)
+
 
 # Append the calculated VaR and Wert
-portfolio$Value_at_risk <- var_berechnen
-portfolio$Wert <- wert_berechnen
+#portfolio$Value_at_risk <- var_berechnen
+#portfolio$Wert <- 
 
 # Additional
 #portfolio <- portfolio %>% arrange_all(desc)
